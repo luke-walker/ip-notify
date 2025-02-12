acbab67e5a6f71703860306f892be0408f7e0c61
Luke Walker <lukeryanwalker2000@gmail.com>
2025-02-12 11:52:46 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv
+
+def main():
+    load_dotenv()
+    
+    # Fetch external IPv4
+    # ...
+
+    # Compare fetched IP to stored IP
+    # ...
+
+    # Send email if different
+    # ...
+
+    # Store new IP
+    # ...
+
+if __name__ == "__main__":
+    main()
