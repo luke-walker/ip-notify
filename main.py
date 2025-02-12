21d340a5dfd3f402355cec4eec7262176d2470d6
Luke Walker <lukeryanwalker2000@gmail.com>
2025-02-12 12:08:27 -0800

    Load .env variables

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
+import os
+
 from dotenv import load_dotenv
 
 def main():
     load_dotenv()
-    
+
+    def get_env(var: str, req: bool=True) -> str | None:
+        val = os.getenv(var)
+        if val is None and req:
+            print(f"Missing required .env var: {var}")
+            exit(1)
+        return val
+
+    EMAIL_SERVER = get_env("EMAIL_SERVER")
+    EMAIL_PORT = get_env("EMAIL_PORT")
+    EMAIL_ADDRESS = get_env("EMAIL_ADDRESS")
+    EMAIL_PASSWORD = get_env("EMAIL_PASSWORD")
+
     # Fetch external IPv4
     # ...
 
