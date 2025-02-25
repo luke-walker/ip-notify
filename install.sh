08c34f310a6d407126601ddd027d71aa11a8dad3
Luke Walker <lukeryanwalker2000@gmail.com>
2025-02-24 22:32:31 -0800

    Prompt user for missing .env vars
    Install script will keep previously declared .env vars

--- install.sh
+++ install.sh
@@ -1,22 +1,39 @@
 #!/bin/bash
 
-BASEDIR=$(dirname "$0")
+BASE_DIR=$(dirname "$0")
+INSTALL_DIR="/opt/ip-notify"
 
 # Create installation directory
-if ! [[ -d "/opt/ip-notify" ]] ; then
-    sudo mkdir /opt/ip-notify
-    sudo chmod g=wxr /opt/ip-notify
-    sudo chown www-data:www-data /opt/ip-notify
+if ! [[ -d ${INSTALL_DIR} ]] ; then
+    sudo mkdir ${INSTALL_DIR}
+    sudo chown www-data:www-data ${INSTALL_DIR}
+    sudo chmod og=wxr ${INSTALL_DIR}
 fi
 
 # Install script and dependencies
-if ! [[ -f "/opt/ip-notify/.env" ]] ; then
-	cp ${BASEDIR}/.env.example /opt/ip-notify/.env
-	echo "Edit /opt/ip-notify/.env before running the script."
-fi
-cp ${BASEDIR}/main.py /opt/ip-notify/main.py
-rm -rf /opt/ip-notify/env
-python -m venv /opt/ip-notify/env
-/opt/ip-notify/env/bin/python -m pip install -r ${BASEDIR}/requirements.txt
-sudo chown -R www-data:www-data /opt/ip-notify
-sudo chmod -R og=wxr /opt/ip-notify
+cp ${BASE_DIR}/main.py ${INSTALL_DIR}/main.py
+rm -rf ${INSTALL_DIR}/.venv
+python -m venv ${INSTALL_DIR}/.venv
+${INSTALL_DIR}/.venv/bin/python -m pip install -r ${BASE_DIR}/requirements.txt
+sudo chown -R www-data:www-data ${INSTALL_DIR}
+sudo chmod -R og=wxr ${INSTALL_DIR}
+
+# Fill .env file
+missing_vars=()
+touch ${INSTALL_DIR}/.env
+while IFS="=" read exmp_var _ ; do
+    missing=1
+    while IFS="=" read user_var _ ; do
+        if [[ ${exmp_var} == ${user_var} ]] ; then
+            missing=0
+            break
+        fi
+    done < ${INSTALL_DIR}/.env
+    if [[ ${missing} -eq 1 ]] ; then
+        missing_vars+=(${exmp_var})
+    fi
+done < ${BASE_DIR}/.env.example
+for var in ${missing_vars[@]} ; do
+    read -p "Enter ${var} value: " user_val
+    echo "${var}=${user_val}" >> ${INSTALL_DIR}/.env
+done
